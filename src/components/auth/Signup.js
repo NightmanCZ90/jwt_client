571a27fccd57a2ea1bf71ed3eb65c3e36eef1241
NightmanCZ90 <novak.jan90@gmail.com>
2022-01-23 19:09:59 +0100

    feat: Setup store and create Signup component

---
+++ src/components/auth/Signup.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+const Signup = () => {
+  return (
+    <form action="">
+      <fieldset>
+        <label htmlFor="">Email</label>
+        <Field
+          name="email"
+          type="text"
+          component="input"
+          autoComplete="none"
+        />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="">Password</label>
+        <Field
+          name="password"
+          type="password"
+          component="input"
+          autoComplete="none"
+        />
+      </fieldset>
+    </form>
+  )
+}
+
+export default reduxForm({ form: 'signup' })(Signup);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+import reducers from './reducers';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Routes>
-        <Route path="/" exact element={<Welcome />} />
-      </Routes>
-    </App>
-  </BrowserRouter>
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" exact element={<Welcome />} />
+          <Route path="/signup" exact element={<Signup />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
+  </Provider>
   , document.querySelector('#root'));
