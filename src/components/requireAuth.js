ca77a45388eb2fb7c032bf0d9c85f241ea269815
NightmanCZ90 <novak.jan90@gmail.com>
2022-01-23 21:54:20 +0100

    data: Create requireAuth HOC

---
+++ src/components/requireAuth.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from 'actions'
+
+export default function requireAuth(ChildComponent) {
+  const ComposedComponent = (props) => {
+    const navigate = useNavigate();
+    const { auth } = props
+
+    const shouldNavigateAway = () => {
+      if (!auth) {
+        navigate('/')
+      }
+    }
+  
+    useEffect(() => {
+      shouldNavigateAway()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [auth])
+
+    return <ChildComponent {...props} />
+  }
+
+  function mapState(state) {
+    return { auth: state.auth.authenticated }
+  }
+
+  return connect(mapState, actions)(ComposedComponent)
+}
